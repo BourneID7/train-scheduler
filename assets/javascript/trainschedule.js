0f6f64cb573fc4095afb21a48a4ed0a56c84fb72
Jason Bourne <m.bourne@wi.rr.com>
2019-05-11 17:20:36 -0500

    initial commit, html page setup

---
+++ assets/javascript/trainschedule.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCvNY91JtpiMZFsMUFi_I5VUsxnYnGTmhs",
+    authDomain: "train-schedule-bdbc6.firebaseapp.com",
+    databaseURL: "https://train-schedule-bdbc6.firebaseio.com",
+    projectId: "train-schedule-bdbc6",
+    storageBucket: "train-schedule-bdbc6.appspot.com",
+    messagingSenderId: "793879847649",
+    appId: "1:793879847649:web:cbb2487a16fc4ea4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
