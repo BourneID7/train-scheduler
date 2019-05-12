3f0e5256c75536200b0626d9f053685c2a93e4c4
Jason Bourne <m.bourne@wi.rr.com>
2019-05-11 19:21:46 -0500

    connected to firebase database

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyCvNY91JtpiMZFsMUFi_I5VUsxnYnGTmhs",
     authDomain: "train-schedule-bdbc6.firebaseapp.com",
@@ -10,3 +10,38 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var trainName;
+  var destination;
+  var frequency;
+  var nextArrival;
+  var minutesAway;
+
+  // on click submit button add form values to firebase
+
+  $("#submit").on("click", function(event){
+      event.preventDefault();
+
+      trainName = $("#trainName").val().trim();
+      destination = $("#destination").val().trim();
+      firstTrain = $("#firstTrain").val().trim();
+      frequency = $("#frequency").val().trim();
+
+      database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+      })
+
+      database.ref().on("child_added", function(snapshot) {
+          console.log(snapshot.val().trainName);
+          console.log(snapshot.val().destination);
+          console.log(snapshot.val().firstTrain);
+          console.log(snapshot.val().frequency);
+
+      })
+
+  })
