677f8b9c0338b30cbbed4b4299e27b150ce1b6ea
Jason Bourne <m.bourne@wi.rr.com>
2019-05-15 07:37:03 -0500

    separated form submit function from database snapshot function to fix duplication of chart rows when form submitted without reloading page

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -1,99 +1,108 @@
 // Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCvNY91JtpiMZFsMUFi_I5VUsxnYnGTmhs",
-    authDomain: "train-schedule-bdbc6.firebaseapp.com",
-    databaseURL: "https://train-schedule-bdbc6.firebaseio.com",
-    projectId: "train-schedule-bdbc6",
-    storageBucket: "train-schedule-bdbc6.appspot.com",
-    messagingSenderId: "793879847649",
-    appId: "1:793879847649:web:cbb2487a16fc4ea4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCvNY91JtpiMZFsMUFi_I5VUsxnYnGTmhs",
+  authDomain: "train-schedule-bdbc6.firebaseapp.com",
+  databaseURL: "https://train-schedule-bdbc6.firebaseio.com",
+  projectId: "train-schedule-bdbc6",
+  storageBucket: "train-schedule-bdbc6.appspot.com",
+  messagingSenderId: "793879847649",
+  appId: "1:793879847649:web:cbb2487a16fc4ea4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
-
-  var trainName;
-  var destination;
-  var frequency;
-  var nextArrival;
-  var minutesAway;
-  
-
-  // on click submit button add form values to firebase
-
-  $("#submit").on("click", function(event){
-      event.preventDefault();
-
-      trainName = $("#trainName").val().trim();
-      destination = $("#destination").val().trim();
-      firstTrain = $("#firstTrain").val().trim();
-      frequency = $("#frequency").val().trim();
-
-      database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency
-      })
-
-      database.ref().on("child_added", function(snapshot) {
-          console.log(snapshot.val().trainName);
-          console.log(snapshot.val().destination);
-          console.log("first train start time is " + snapshot.val().firstTrain);
-          console.log("train arrives every " + snapshot.val().frequency + " minutes");
-
-        // time variable calculations
-
-        var today = new Date();
-        var currentTime = today.getHours() + ":" + today.getMinutes();
-        console.log("current time is " + currentTime);
-
-        // convert current time to total minutes from midnight for easier math
-        var currentTimeMinutes = (today.getHours() * 60) + (today.getMinutes());
-        console.log("current time in minutes is " + currentTimeMinutes);
-
-        var startTimeMinutes = moment.duration(firstTrain).asMinutes();
-        console.log("first train start time in minutes is " + startTimeMinutes);
-
-        var numberTrains = Math.floor((currentTimeMinutes - startTimeMinutes) / (snapshot.val().frequency));
-        console.log("number of trains run until now is " + numberTrains);
-
-        var nextArrival = startTimeMinutes + ((numberTrains + 1) * snapshot.val().frequency);
-        console.log("last train arrived at " + nextArrival + " minutes into the day")
-
-        var convertedArrivalTime = moment().startOf('day').add(nextArrival, 'minutes').format('hh:mm A');
-        console.log(convertedArrivalTime);
-
-        var minutesAway = nextArrival - currentTimeMinutes;
-        console.log("the next train is due in " + minutesAway + " minutes");
+// global variables
+var database = firebase.database();
+var trainName;
+var destination;
+var frequency;
+var firstTrain;
+var nextArrival;
+var minutesAway;
 
 
-  
-        // function to add row to train schedule chart on submit
+// on click submit button add form values to firebase
 
-        function newRow() {
-            var row = $('<tr>');
-            var tdName = $('<td id="trainName">').text(snapshot.val().trainName);
-            var tdDest = $('<td id="destination">').text(snapshot.val().destination);
-            var tdFreq = $('<td id="frequency">').text(snapshot.val().frequency);
-            var tdNext = $('<td id="nextTrain">').text(convertedArrivalTime);
-            var tdMinutes = $('<td id="minutes">').text(minutesAway);
-            $("#currentTrains").append(row);
-            row.append(tdName);
-            row.append(tdDest);
-            row.append(tdFreq);
-            row.append(tdNext);
-            row.append(tdMinutes);
-        } 
-        newRow();
+$("#submit").on("click", function(event){
+    event.preventDefault();
 
-      // sort rows by next train to arrive
+    // get user input
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#firstTrain").val().trim();
+    frequency = $("#frequency").val().trim();
 
-      // clear form
-      $("#addTrain")[0].reset();
+    // push user input values to database
+    database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    })
 
+    // clear form
+    $("#trainName").text("");
+    $("#destination").text("")
+    $("#firstTrain").text("")
+    $("#frequency").text("")
+    $("#addTrain")[0].reset();
 
-      })
+  });
+
+  // firebase event to add row to table 
+  database.ref().on("child_added", function(childSnapshot) {
+
+    // store everything to variables
+    trainName = childSnapshot.val().trainName;
+    destination = childSnapshot.val().destination;
+    frequency = childSnapshot.val().frequency;
+    firstTrain = childSnapshot.val().firstTrain;
+
+    // console log all variables
+    console.log(trainName);
+    console.log(destination);
+    console.log("first train start time is " + firstTrain);
+    console.log("train arrives every " + frequency + " minutes");
+
+    // time variable calculations
+
+    var today = new Date();
+    var currentTime = today.getHours() + ":" + today.getMinutes();
+    console.log("current time is " + currentTime);
+
+    // convert current time to total minutes from midnight for easier math
+    var currentTimeMinutes = (today.getHours() * 60) + (today.getMinutes());
+    console.log("current time in minutes is " + currentTimeMinutes);
+
+    var startTimeMinutes = moment.duration(firstTrain).asMinutes();
+    console.log("first train start time in minutes is " + startTimeMinutes);
+
+    var numberTrains = Math.floor((currentTimeMinutes - startTimeMinutes) / frequency);
+    console.log("number of trains run until now is " + numberTrains);
+
+    var nextArrival = startTimeMinutes + ((numberTrains + 1) * frequency);
+    console.log("last train arrived at " + nextArrival + " minutes into the day")
+
+    var convertedArrivalTime = moment().startOf('day').add(nextArrival, 'minutes').format('hh:mm A');
+    console.log(convertedArrivalTime);
+
+    var minutesAway = nextArrival - currentTimeMinutes;
+    console.log("the next train is due in " + minutesAway + " minutes");
+
+    // function to add row to train schedule chart on submit
+    // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(convertedArrivalTime),
+    $("<td>").text(minutesAway)
+  );
+
+  // Append the new row to the table
+  $("#currentTrains").append(newRow);
+
+  // sort rows by next train to arrive
 
   })
+
